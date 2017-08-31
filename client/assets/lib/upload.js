d395b6812c853a6e6b346dfe895862d1b959b801
kokoh <kokohtan@gmail.com>
2017-08-31 14:48:42 +0700

    mvp dapet

--- client/assets/lib/upload.js
+++ client/assets/lib/upload.js
@@ -15,8 +15,9 @@ const app = new Vue({
     message: 'Hello',
     currentStatus: null,
     uploadError: null,
-    uploadFieldName: 'photos',
-    uploadedFiles: []
+    uploadFieldName: 'image',
+    uploadedFiles: [],
+    files:[]
   },
 
   computed: {
@@ -36,6 +37,22 @@ const app = new Vue({
 
   methods: {
     // reset form to initial state
+
+    getAllFile () {
+      let self = this
+      axios.get('http://localhost:3000/files')
+      .then(response => {
+        self.files = response.data
+      })
+    }, // getAllFile()
+
+    deleteFile (id) {
+      axios.delete(`http://localhost:3000/files/${id}`)
+      .then(r => {
+        this.files.splice(id, 1)
+      })
+    },
+
     reset() {
       this.currentStatus = STATUS_INITIAL;
       this.uploadedFiles = [];
@@ -48,13 +65,10 @@ const app = new Vue({
       console.log('form data', formData)
       this.currentStatus = STATUS_SAVING;
 
-      axios.post('http://localhost:3000/files/add', {
-        data: {
-          image: formData
-        }
-      })
+      axios.post('http://localhost:3000/files/add', formData)
       .then(up => {
-        console.log('up', up)
+        // console.log('up', up)
+        this.uploadedFiles = [].concat(up)
         this.currentStatus = STATUS_SUCCESS
       })
       .catch(e => {
@@ -67,15 +81,8 @@ const app = new Vue({
     filesChange(fieldName, fileList) {
       // handle file changes
       const formData = new FormData();
-      if (!fileList.length) return;
-      // append the files to FormData
-      Array
-        .from(Array(fileList.length).keys())
-        .map(x => {
-          formData.append(fieldName, fileList[x], fileList[x].name);
-        });
-      // save it
-      // console.log('form data', formData)
+      formData.append('image', fileList[0])
+      console.log(formData)
       this.save(formData);
     }  // filesChange()
 
@@ -86,102 +93,7 @@ const app = new Vue({
   },
 
   created () {
-
+    this.getAllFile()
   }
 
 })
-
-
-
-
-
-/*
- ----------------------------------  backup  -------------------------------------------
-
-
-// untuk status uploader
-const STATUS_INITIAL = 0
-const STATUS_SAVING = 1
-const STATUS_SUCCESS = 2
-const STATUS_FAILED = 3
-
-// base url api
-const BASE_URL = 'http://localhost:3000'
-
-const app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello',
-    currentStatus: null,
-    uploadError: null,
-    uploadFieldName: 'photos',
-    uploadedFiles: []
-  },
-
-  computed: {
-    isInitial() {
-      return this.currentStatus === STATUS_INITIAL;
-    },
-    isSaving() {
-      return this.currentStatus === STATUS_SAVING;
-    },
-    isSuccess() {
-      return this.currentStatus === STATUS_SUCCESS;
-    },
-    isFailed() {
-      return this.currentStatus === STATUS_FAILED;
-    }
-  },  // computed
-
-  methods: {
-    // reset form to initial state
-    reset() {
-      this.currentStatus = STATUS_INITIAL;
-      this.uploadedFiles = [];
-      this.uploadError = null;
-    },  // reset()
-
-    // upload data to the server
-    save(formData) {
-      this.currentStatus = STATUS_SAVING;
-      // const url = `${BASE_URL}/photos/upload`;
-      upload(formData)
-        .then(wait(1500)) // DEV ONLY: wait for 1.5s
-        .then(x => {
-          this.uploadedFiles = [].concat(x);
-          this.currentStatus = STATUS_SUCCESS;
-        })
-        .catch(err => {
-          this.uploadError = err.response;
-          this.currentStatus = STATUS_FAILED;
-        });
-    },  // save()
-
-    filesChange(fieldName, fileList) {
-      // handle file changes
-      const formData = new FormData();
-      if (!fileList.length) return;
-      // append the files to FormData
-      Array
-        .from(Array(fileList.length).keys())
-        .map(x => {
-          formData.append(fieldName, fileList[x], fileList[x].name);
-        });
-      // save it
-      this.save(formData);
-    }  // filesChange()
-
-  },  // methods
-
-  mounted() {
-    this.reset()
-  },
-
-  created() {
-
-  }
-
-})
-
-
-*/
