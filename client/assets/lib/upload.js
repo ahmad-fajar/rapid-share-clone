71789ecf9ad9e5ea29b852990d658381721f35b8
ahmad-fajar <shin.fajar@gmail.com>
2017-08-31 10:55:37 +0700

    commit dulu, biar bisa test

--- client/assets/lib/upload.js
+++ client/assets/lib/upload.js
@@ -1,13 +1,22 @@
 'use strict'
 
+// untuk status uploader
+const STATUS_INITIAL = 0
+const STATUS_SAVING  = 1
+const STATUS_SUCCESS = 2
+const STATUS_FAILED  = 3
+
+// base url api
+const BASE_URL = 'http://localhost:3000'
+
 const app = new Vue({
   el: '#app',
   data: {
     message: 'Hello',
     currentStatus: null,
     uploadError: null,
-    uploadFieldName: 'photos'
-    uploadedFiles: [],
+    uploadFieldName: 'photos',
+    uploadedFiles: []
   },
 
   computed: {
@@ -36,7 +45,100 @@ const app = new Vue({
     // upload data to the server
     save(formData) {
       this.currentStatus = STATUS_SAVING;
-      const url = `${BASE_URL}/photos/upload`;
+      // const url = `${BASE_URL}/photos/upload`;
+      upload(formData)
+      axios.
+        .then(wait(1500)) // DEV ONLY: wait for 1.5s
+        .then(x => {
+          this.uploadedFiles = [].concat(x);
+          this.currentStatus = STATUS_SUCCESS;
+        })
+        .catch(err => {
+          this.uploadError = err.response;
+          this.currentStatus = STATUS_FAILED;
+        });
+    },  // save()
+
+    filesChange(fieldName, fileList) {
+      // handle file changes
+      const formData = new FormData();
+      if (!fileList.length) return;
+      // append the files to FormData
+      Array
+        .from(Array(fileList.length).keys())
+        .map(x => {
+          formData.append(fieldName, fileList[x], fileList[x].name);
+        });
+      // save it
+      this.save(formData);
+    }  // filesChange()
+
+  },  // methods
+
+  mounted () {
+    this.reset()
+  },
+
+  created () {
+
+  }
+
+})
+
+
+
+
+
+/*
+ ----------------------------------  backup  -------------------------------------------
+
+
+// untuk status uploader
+const STATUS_INITIAL = 0
+const STATUS_SAVING = 1
+const STATUS_SUCCESS = 2
+const STATUS_FAILED = 3
+
+// base url api
+const BASE_URL = 'http://localhost:3000'
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello',
+    currentStatus: null,
+    uploadError: null,
+    uploadFieldName: 'photos',
+    uploadedFiles: []
+  },
+
+  computed: {
+    isInitial() {
+      return this.currentStatus === STATUS_INITIAL;
+    },
+    isSaving() {
+      return this.currentStatus === STATUS_SAVING;
+    },
+    isSuccess() {
+      return this.currentStatus === STATUS_SUCCESS;
+    },
+    isFailed() {
+      return this.currentStatus === STATUS_FAILED;
+    }
+  },  // computed
+
+  methods: {
+    // reset form to initial state
+    reset() {
+      this.currentStatus = STATUS_INITIAL;
+      this.uploadedFiles = [];
+      this.uploadError = null;
+    },  // reset()
+
+    // upload data to the server
+    save(formData) {
+      this.currentStatus = STATUS_SAVING;
+      // const url = `${BASE_URL}/photos/upload`;
       upload(formData)
         .then(wait(1500)) // DEV ONLY: wait for 1.5s
         .then(x => {
@@ -65,11 +167,15 @@ const app = new Vue({
 
   },  // methods
 
-  mounted: {
+  mounted() {
     this.reset()
   },
 
-  created: {
+  created() {
 
   }
+
 })
+
+
+*/
